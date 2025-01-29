7c59e99b4ae84556d4beb222de102b3341100a30
Mitch Palczewski <135446256+mitchp819@users.noreply.github.com>
2025-01-29 13:22:18 -0500

    mmore cool stuff

---
+++ app/brush.py
@@ -0,0 +1,59 @@
+from app.drawing_canvas import DrawingCanvasGUI
+
+class BasicBrush: 
+    """
+    Handles Canvas Events
+    """
+    def __init__(self, drawing_canvas: DrawingCanvasGUI,):
+        self.drawing_canvas = drawing_canvas
+        self.canvas = self.drawing_canvas.canvas
+        self.canvas.bind("<Button-1>", self.on_mouse_down)
+        self.canvas.bind("<B1-Motion>", self.create_mark)
+        self.canvas.bind("<ButtonRelease-1>", self.on_mouse_released)
+        
+    def on_mouse_down(self, event):
+        self.create_mark(event)
+        pass
+
+    def create_mark(self, event):
+        rectangle = RectangleMark().create_canvas_mark(event.x, event.y, self.drawing_canvas)
+        RectangleMark().create_data_mark(rectangle, self.drawing_canvas)
+
+    def on_mouse_released(self, event):
+        pass   
+
+
+class RectangleMark:
+    """
+    Creates Rectangle on Canvas
+    """
+    @staticmethod
+    def create_canvas_mark(x: int, y: int, drawing_canvas: DrawingCanvasGUI):
+        color = drawing_canvas.interface.color_hex
+        size = drawing_canvas.interface.size 
+        canvas = drawing_canvas.canvas
+
+        rectangle = canvas.create_rectangle(x, y, (x + 1), (y + 1), fill= color, width= size)
+        return rectangle
+        
+    @staticmethod
+    def create_data_mark(rectangle: int, drawing_canvas: DrawingCanvasGUI):
+        scalor = drawing_canvas.scalor
+        canvas = drawing_canvas.canvas
+        color = drawing_canvas.interface.color
+        width = drawing_canvas.interface.width
+        height = drawing_canvas.interface.height
+        canvas_data = drawing_canvas.interface.canvas_data
+        stroke_data = drawing_canvas.interface.stroke_data
+        
+        x1, y1, x2, y2 = canvas.bbox(rectangle)
+        x1 = int(x1 // scalor)
+        y1 = int(y1 // scalor)
+        x2 = int(x2 // scalor)
+        y2 = int(y2 // scalor)
+        
+        for x in range(x1, x2):
+            for y in range(y1, y2):
+                if (x < width and x >= 0 and y < height and y >= 0):
+                    canvas_data[y, x] = color
+                    stroke_data[y, x] = color
--- app/drawing_canvas.py
+++ app/drawing_canvas.py
@@ -1,153 +1,95 @@
 import tkinter as tk
-from tkinter import ttk
-from typing import Literal
 import numpy as np
 import pandas as pd
-from PIL import Image
-import os
-import re
+import time
 
-try:
-    from ctypes import windll
-    windll.shcore.SetProcessDpiAwareness(1)
-except ImportError:
-    print("Error: windll not imported. Text may be blurred")
-    pass
+from brush import *
 
-from app import (  ImageProcessor, BrushTool, AppConsole, greyscale_value_to_hex, shape_img,
-                  get_a_DATABASE, get_LOADED_DB, get_last_file_by_id, 
-                  canvas_np_img_to_png, UI_COLOR, ASSETS_DIR, DATA_DIR, 
-                  SIMILAR_IMAGES_DIR
-)
+WHITE_VALUE = 255
+STROKE_DEFAULT_VALUE = -1
 
-class DrawingData():
-    MAX_MARK_HISTORY = 10
-    STROKE_DEAFULT_VALUE = -1
-    WHITE_VALUE = 255
-    def __init__(self, image_width: int = 128, image_height: int = 128):
-        """
-        Holds the canvas data.
-        Args:
-            image_width: int        width of the image
-            image_height: int       height of the image
-        """
-        self.image_width = image_width
-        self.image_height = image_height
-        self.data = {
-            "canvas":       [],
-            "stroke":       [],
-            "line_type":    [],
-            "speed":        [],
-            "ratio":        [],
-            "color_type":   [],
-        }
-        self.dataframe = pd.DataFrame(self.data)
-        self.canvas_data = self.white_canvas()
-        self.last_save_canvas_data = self.white_canvas()
-        self.stroke_data = self.blank_stroke()
-        self.prev_stroke_data = []
+
+class DrawingCanvasInterface:
+    """
+    Stores Data for the Drawing Canvas
+    """
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+        self.color = 0
+        self.color_hex = 'black'
+        self.size = 4
+        self.canvas_data = np.ones((width, height), dtype= np.uint8) * WHITE_VALUE
+        self.stroke_data = np.full((width, height), fill_value= STROKE_DEFAULT_VALUE, dtype=np.int8)
         pass
-
-    def white_canvas(self):
-        return np.ones((self.image_width, self.image_height), dtype= np.uint8) * self.WHITE_VALUE
-   
-    def blank_stroke(self):
-        return np.full((self.image_width, self.image_height), fill_value= self.STROKE_DEAFULT_VALUE, dtype=np.uint8)
-
-    def create_mark(self, x1: int, y1: int, x2: int, y2: int, brush_color: str):
-        """
-        Adds mark data to canvas_data and stroke_data.
-
-        Args:
-            x1: int             left bound
-            y1: int             top bound
-            x2: int             right bound
-            y2: int             bottom bound
-            brush_color: str    color of mark
-        """
-        if len(self.prev_stroke_data) > self.MAX_MARK_HISTORY:
-            self.prev_stroke_data.pop()
-        self.prev_stroke_data.append(self.stroke_data.copy())
-
-        for x in range(x1, x2):
-            for y in range(y1, y2):
-                if (x < self.image_width and x >= 0 and y < self.image_height and y >= 0):
-                    self.canvas_data[y, x] = brush_color
-                    self.stroke_data[y, x] = brush_color 
-        
-        pass
-
-    def save_stroke_to_dataset(self):
-        """
-        Saves (canvas, stroke, attributes 1, ..., attribute n) data to dataset.
-        """
-
-
-        pass
-
-class DrawingCanvas(ttk.Frame):
-    def __init__(self, container: ttk.Frame, frame_scalor: int = 6, image_width: int = 128, image_height: int = 128):
-        """
-        Holds the drawing canvas.
-
-        Args:
-            container: ttk.Frame    parent frame
-            frame_scalor: int       scales the frame size
-            image_width: int        width of the image
-            image_height: int       height of the image
-        """
-        super().__init__(container)
-        self.container = container
-        self.frame_scalor = frame_scalor
-        self.image_width = image_width
-        self.image_height = image_height
-        self.frame_width = self.image_width * self.frame_scalor
-        self.frame_height = self.image_height * self.frame_scalor
-        self.brush_tool = None
-        self.data_gather_tool = None
-        self.app_console = None
-        self.gen_tool = None
-        self.info_pane = None
-        
-        main_frame = tk.Frame(self,border=3, relief='raised', bg=UI_COLOR)
-        main_frame.pack()
-        
-        self.canvas = tk.Canvas(main_frame, 
-                                width=self.frame_width, 
-                                height=self.frame_height, 
-                                bg='white',
-                                border=2,
-                                relief='groove'
-                                )
-        self.canvas.pack(pady=10,padx=10)
-        self.pack(side=tk.LEFT, expand= True)
-
-        self.drawing_data = DrawingData(image_width=self.image_width, image_height=self.image_height)
-
-        self.canvas.bind("<Button-1>", self.on_mouse_down)
-        self.canvas.bind("<B1-Motion>", self.create_mark)
-        self.canvas.bind("<ButtonRelease-1>", self.on_mouse_released)
-        pass
+      
+class DrawingCanvasGUI:
+    """
+    Configures Tkinter Drawing Canvas 
+    """
+    def __init__(self, container: tk.Frame, interface: DrawingCanvasInterface, scalor):
+        self.interface = interface
+        self._scalor = scalor
+        self.gui_width = self.interface.width * self.scalor
+        self.gui_height = self.interface.height * self.scalor
+        self.canvas = tk.Canvas(master=container, width=self.gui_width, height=self.gui_height, bg='white')
+        BasicBrush(self)
+        self.canvas.pack()
     
-    def set_brush_tool(self, brush_tool: BrushTool):
-        self.brush_tool = brush_tool
-    def set_data_gather_tool(self, data_gather_tool):
-        self.data_gather_tool = data_gather_tool
-    def set_app_console(self, app_console):
-        self.app_console = app_console
-    def set_gen_tool(self, gen_tool):
-        self.gen_tool = gen_tool
-    def set_info_pane(self, info_pane):
-        self.info_pane = info_pane
-
-    def on_mouse_down(self, event):
-        """
-        Records the initial mouse click position.
-        """
+    @property
+    def scalor(self):
+        return self._scalor
+    
+    @scalor.setter
+    def set_scalor(self, value):
+        self._scalor = value
+        self.gui_width = self.interface.width * self._scalor
+        self.gui_height = self.interface.height * self._scalor
+        self.canvas.config(width = self.gui_width, height = self.gui_height)
         pass
 
-    def on_mouse_released(self, event):
-        if self.data_gather_tool.get_data_gather_mode() == 'auto':
-            self.save_stroke_to_dataset()   
+    def load_np_image(self, image: np):
+        if image.shape != (self.gui_width, self.gui_height):
+            self.scale_np_image_to_canvas(image)
+            pass
+        for row, column_array in enumerate(image):
+            for col, pixel_value in enumerate(column_array):
+                hex_color = Color.greyscale_value_to_hex(pixel_value)
+                self.canvas.create_rectangle(
+                    col, row, (col+1),
+                    (row+1), fill=hex_color)
         pass
+
+    def scale_np_image_to_canvas(self, image: np):
+        pass
+
+class Color():
+    def hex_to_greyscale_value(hex_color: str):
+        hex_color = hex_color.lstrip('#')
+        r = int(hex_color[0:2], 16)
+        g = int(hex_color[2:4], 16)
+        b = int(hex_color[4:6], 16)
+        grayscale_value = round((r + g + b) / 3)
+        return grayscale_value
+    
+    def greyscale_value_to_hex(value):
+        return f"#{value:02x}{value:02x}{value:02x}"
+
+
+
+
+
+
+if __name__ == "__main__":
+    test_root = tk.Tk()
+    test_root.geometry("800x600")
+    test_root.config(bg = 'blue')
+    test_frame = tk.Frame(test_root)
+   
+    interface = DrawingCanvasInterface(width=400, height=400)
+    canvas = DrawingCanvasGUI(test_frame, interface, 1)
+    
+    test_frame.pack()
+    test_root.mainloop()
+
 
